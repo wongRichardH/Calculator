280611e38fb5fa87388db625bacea6cf5552b241
Sophie Zhou <soph.zhou@gmail.com>
2017-09-19 11:18:21 -0700

    Initial Commit.

--- BaseExam/ViewController.swift
+++ BaseExam/ViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var aButton: UIButton!
+    @IBOutlet weak var bButton: UIButton!
+    @IBOutlet weak var cButton: UIButton!
+    @IBOutlet weak var dButton: UIButton!
+    @IBOutlet weak var eButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.aButton.addTarget(self, action: #selector(aButtonPressed), for: UIControlEvents.touchUpInside)
+        self.bButton.addTarget(self, action: #selector(bButtonPressed), for: UIControlEvents.touchUpInside)
+        self.cButton.addTarget(self, action: #selector(cButtonPressed), for: UIControlEvents.touchUpInside)
+        self.dButton.addTarget(self, action: #selector(dButtonPressed), for: UIControlEvents.touchUpInside)
+        self.eButton.addTarget(self, action: #selector(eButtonPressed), for: UIControlEvents.touchUpInside)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func aButtonPressed() {
+        let vc = AViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
     }
 
+    @objc func bButtonPressed() {
+        let vc = BViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+
+    @objc func cButtonPressed() {
+        let vc = CViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+
+    @objc func dButtonPressed() {
+        let vc = DViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+
+    @objc func eButtonPressed() {
+        let vc = EViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
 
