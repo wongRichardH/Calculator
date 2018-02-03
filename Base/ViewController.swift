fcede48d3af1d492171b861296a47f88dbe338ec
Sophie Zhou <soph.zhou@gmail.com>
2018-02-02 17:08:35 -0800

    Create the Shopping App.

--- Base/ViewController.swift
+++ Base/ViewController.swift
@@ -10,44 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var aButton: UIButton!
-    @IBOutlet weak var bButton: UIButton!
-    @IBOutlet weak var cButton: UIButton!
-    @IBOutlet weak var dButton: UIButton!
-    @IBOutlet weak var eButton: UIButton!
-
     override func viewDidLoad() {
-        super.viewDidLoad()
-        self.aButton.addTarget(self, action: #selector(aButtonPressed), for: UIControlEvents.touchUpInside)
-        self.bButton.addTarget(self, action: #selector(bButtonPressed), for: UIControlEvents.touchUpInside)
-        self.cButton.addTarget(self, action: #selector(cButtonPressed), for: UIControlEvents.touchUpInside)
-        self.dButton.addTarget(self, action: #selector(dButtonPressed), for: UIControlEvents.touchUpInside)
-        self.eButton.addTarget(self, action: #selector(eButtonPressed), for: UIControlEvents.touchUpInside)
-    }
-
-    @objc func aButtonPressed() {
-        let vc = AViewController()
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-
-    @objc func bButtonPressed() {
-        let vc = BViewController()
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-
-    @objc func cButtonPressed() {
-        let vc = CViewController()
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-
-    @objc func dButtonPressed() {
-        let vc = DViewController()
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-
-    @objc func eButtonPressed() {
-        let vc = EViewController()
-        self.navigationController?.pushViewController(vc, animated: true)
     }
 
 }
